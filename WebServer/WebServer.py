48b3cb01d6e766b4fd895fc95758a998b9a29711
root <root@ip-172-31-28-150.us-west-2.compute.internal>
2016-04-13 06:05:21 +0000

    WebServer and Related HTML files

---
+++ WebServer/WebServer.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, request, url_for, render_template, Response
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template('home_page.html')
+
+@app.route("/Login", methods=["POST"])
+def login_page():
+    if requests.method == "POST":
+	login_data = request.form
+	# if(verify_user(login_data))
+	if login_data['userType'] == '0':
+		return redirect(url_for('administrator_page'))
+        if login_data['userType'] == '1':
+		return redirect(url_for('user_page'))
+	return "Please enter the type of the user"
+
+@app.route("/Administrator", methods=["GET", "POST"])
+def administrator_page():
+	return "hello administrator"
+
+@app.route("/User", methods=["GET", "POST"])
+def user_page():
+	return "hello User"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.Thread = True
+    app.run('172.31.28.150', 8080)
